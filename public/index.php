928234724399e92923d71a1208451cd7545102a2
PAULO CESAR <paulocesardev.com@gmail.com>
2024-10-02 23:03:11 -0300

    devcontainer

--- public/index.php
+++ public/index.php
@@ -6,20 +6,27 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-include_once realpath(__DIR__.'/../vendor').'/autoload.php';
+include_once realpath(__DIR__ . '/../vendor') . '/autoload.php';
 
-define('PATH_APP',realpath(__DIR__.'/../app').'/');
-define('PATH_VIEW',PATH_APP.'Views/');
+define('PATH_APP', realpath(__DIR__ . '/../app') . '/');
+define('PATH_VIEW', PATH_APP . 'Views/');
 
-$dir = dirname(explode('?', $_SERVER['REQUEST_URI'])[0]) . '/';
+echo '<pre>';
+$r = dirname($_SERVER['SCRIPT_NAME']);
+if ($r == '/') {
+    $dir = explode('?', $_SERVER['REQUEST_URI'])[0];
+} else {
+    $p = '/^' . preg_quote($r, '/') . '/';
+    $dir = preg_replace($p, '', $_SERVER['REQUEST_URI'], 1);
+    $dir = explode('?', $dir)[0];
+}
+$dir = trim($dir, '/') . '/';
 $dir = str_replace('//', '/', $dir);
-if ($dir == '/')
-    $file =  'Home';
-else
-    $file = trim(explode('/', substr($_SERVER['REQUEST_URI'], strlen($dir)))[0] ?? '');
+$file = $dir == '/' ? 'Home' : explode('/', $dir)[0];;
 
 empty(strpos($file, '.php')) && $file .= '.php';
 
 App\Models\Base::initializeDB();
 
-include realpath(__DIR__.'/../app/Controllers').'/'.ucfirst($file);
+echo ucfirst($file);
+include realpath(__DIR__ . '/../app/Controllers') . '/' . ucfirst($file);
